a1da87db3d0145d7272e5ee2cf96e9bb99a9ea00
JalmaGit <jmbb00@hotmail.com>
2023-03-23 03:59:35 +0100

    Update

--- main.cpp
+++ main.cpp
@@ -8,5 +8,26 @@ int main(){
     Canvas canvas(Canvas::Parameters().size(640,640));
     GLRenderer renderer(canvas);
 
+    auto scene = Scene::create();
+    auto camera = OrthographicCamera::create();
 
+    OrbitControls controls{camera, canvas};
+
+    const auto planeGeometry = PlaneGeometry::create(100, 100);
+    const auto planeMaterial = MeshLambertMaterial::create();
+    planeMaterial->color = Color::gray;
+    planeMaterial->side = DoubleSide;
+    auto plane = Mesh::create(planeGeometry, planeMaterial);
+    plane->rotateX(math::degToRad(90));
+    plane->receiveShadow = true;
+    scene->add(plane);
+
+
+    canvas.onWindowResize([&](WindowSize size) {
+        renderer.setSize(size);
+    });
+
+    canvas.animate([&](float dt) {
+        renderer.render(scene,camera);
+    });
 }
