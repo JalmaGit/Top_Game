8d4109bcaa35659516459da66e28daf65f8698a3
JalmaGit <jmbb00@hotmail.com>
2023-03-23 13:28:08 +0100

    Small Changes

--- main.cpp
+++ main.cpp
@@ -1,33 +1,87 @@
 #include "threepp/threepp.hpp"
 
-#include <iostream>
-
 using namespace threepp;
 
-int main(){
-    Canvas canvas(Canvas::Parameters().size(640,640));
+int main() {
+
+    Canvas canvas{Canvas::Parameters().antialiasing(8)};
     GLRenderer renderer(canvas);
+    renderer.setClearColor(Color::aliceblue);
 
     auto scene = Scene::create();
-    auto camera = OrthographicCamera::create();
+    auto camera = PerspectiveCamera::create(75, canvas.getAspect(), 0.1f, 1000);
+    camera->position.z = 8;
 
     OrbitControls controls{camera, canvas};
 
-    const auto planeGeometry = PlaneGeometry::create(100, 100);
-    const auto planeMaterial = MeshLambertMaterial::create();
-    planeMaterial->color = Color::gray;
-    planeMaterial->side = DoubleSide;
-    auto plane = Mesh::create(planeGeometry, planeMaterial);
-    plane->rotateX(math::degToRad(90));
-    plane->receiveShadow = true;
-    scene->add(plane);
+    TextureLoader loader;
+    auto material = SpriteMaterial::create();
+    material->map = loader.load("bin/data/textures/brick_bump.jpg");
+    material->map->offset.set(0.5, 0.5);
 
+    auto sprites = Group::create();
+    for (int x = -4; x <= 4; x++) {
+        for (int y = -4; y <= 4; y++) {
+            auto sprite = Sprite::create(material);
+            sprite->position.x = static_cast<float>(x);
+            sprite->position.y = static_cast<float>(y);
+            sprites->add(sprite);
+        }
+    }
+    scene->add(sprites);
+
+    auto helper = Mesh::create(SphereGeometry::create(0.1));
+    scene->add(helper);
 
     canvas.onWindowResize([&](WindowSize size) {
+        camera->aspect = size.getAspect();
+        camera->updateProjectionMatrix();
         renderer.setSize(size);
     });
 
+    Vector2 mouse{-Infinity<float>, -Infinity<float>};
+    MouseMoveListener l([&](auto &pos) {
+        auto size = canvas.getSize();
+        mouse.x = (pos.x / static_cast<float>(size.width)) * 2 - 1;
+        mouse.y = -(pos.y / static_cast<float>(size.height)) * 2 + 1;
+    });
+    canvas.addMouseListener(&l);
+
+    Raycaster raycaster;
     canvas.animate([&](float dt) {
-        renderer.render(scene,camera);
+        helper->visible = false;
+        material->rotation += 1 * dt;
+
+        raycaster.setFromCamera(mouse, camera.get());
+        auto intersects = raycaster.intersectObject(sprites.get(), true);
+        if (!intersects.empty()) {
+            auto &i = intersects.front();
+            helper->position.copy(i.point);
+            helper->visible = true;
+        }
+
+        renderer.render(scene, camera);
     });
 }
+
+/* using namespace threepp;
+
+int main() {
+
+    Canvas canvas(Canvas::Parameters().antialiasing(4).vsync(false));
+    GLRenderer renderer(canvas);
+    renderer.setClearColor(Color::aqua);
+
+    auto scene = Scene::create();
+    auto camera = PerspectiveCamera::create();
+
+    auto planeGeo = PlaneGeometry::create();
+    auto planeMesh = MeshBasicMaterial::create();
+    planeMesh->side = DoubleSide;
+
+    canvas.animate([&](float dt){
+
+        renderer.render(scene, camera);
+
+    });
+} */
