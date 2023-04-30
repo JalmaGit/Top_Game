bfb51cfed81ab4b7cc905fce83dadeb302940bab
JalmaGit <jmbb00@hotmail.com>
2023-04-30 13:07:10 +0200

    updatre

---
+++ include/worldCalculations.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Jalma on 30/04/2023.
+//
+
+#ifndef TOP_GAME_WORLDCALCULATIONS_HPP
+#define TOP_GAME_WORLDCALCULATIONS_HPP
+
+class WorldCalulations {
+public:
+
+    
+
+private:
+};
+
+#endif //TOP_GAME_WORLDCALCULATIONS_HPP
